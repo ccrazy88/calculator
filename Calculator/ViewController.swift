5a6f0086ee3d28b8c4efee7132f09ec62e5b0a71
Chrisna Aing <chrisna@chrisna.org>
2015-02-08 17:30:32 -0800

    Add code given in lectures 1 and 2.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: -
+    // MARK: Private Properties
+
+    private var operandStack = [Double]()
+    private var userIsInTheMiddleOfTypingANumber = false
+
+    // MARK: Computed Properties
+
+    private var displayValue: Double {
+        get {
+            // Directly accessing doubleValue would be too permissive.
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Outlets
+
+    @IBOutlet private weak var display: UILabel!
+
+    // MARK: -
+    // MARK: Utilities
+
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
     }
 
+    private func performOperation(operation: Double -> Double) {
+        if !operandStack.isEmpty {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+
+    // MARK: -
+    // MARK: UI
+
+    @IBAction private func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+
+    @IBAction private func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+
+    @IBAction private func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+    }
 
 }
-
---
+++ README.md
@@ -0,0 +1,17 @@
+# Calculator
+
+## Required Tasks
+
+> Get the Calculator working as demonstrated in lectures 1 and 2. The
+> Autolayout portion at the end of the lecture is extra credit, but give it a
+> try because getting good at autolayout requires experience. If you do not do
+> autolayout, be sure to position everything so that it is visible on all
+> iPhones (i.e. the upper left corner of the scene).
+
+Code is equivalent to the code provided in lecture with the following
+exceptions:
+
+- "MARK" comments added for increased readability
+- Used standard values for autolayout constraints rather than hard-coded value
+  of 8
+- Made all properties and functions private
